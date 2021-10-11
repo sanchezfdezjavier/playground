85f7006a9888a20fb465cb7ca581e482a669cebc
Javier Sanchez <sanchezfdez.javier@gmail.com>
2021-10-11 09:34:06 +0100

    Feat: Different ways of string formatting in python

---
+++ string_formatting.py
@@ -0,0 +1,7 @@
+print("Welcome to string formatting!")
+
+dummy_string = "hahahaha"
+
+print("Hi, my name is {} <---- .format()".format(dummy_string))
+print(f"Hi, my name is {dummy_string}")
+print("Hi, my name is %s"%dummy_string)
